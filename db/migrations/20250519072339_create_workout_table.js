b56957e5c3c717d0249393cbeb42e37783db8d2f
Wentz <ryo_uematsu@mail.toyota.co.jp>
2025-05-21 23:05:19 +0900

    scriptをresetに変更

--- db/migrations/20250519072339_create_workout_table.js
+++ db/migrations/20250519072339_create_workout_table.js
@@ -2,32 +2,31 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    return knex.schema.createTable('workout',function(table){
-        table.increments('id').primary();
-        table.date('workout_day').nullable();
-        table.string('youtube_url');
-        table.integer('workout1').nullable();
-        table.integer('workout2').nullable();
-        table.integer('workout3').nullable();
-        table.integer('workout4').nullable();
-        table.integer('workout5').nullable();
-        table.integer('workout6').nullable();
+exports.up = function (knex) {
+  return knex.schema.createTable('workout', function (table) {
+    table.increments('id').primary();
+    table.date('workout_day').nullable();
+    table.string('youtube_url');
+    table.integer('workout1').nullable();
+    table.integer('workout2').nullable();
+    table.integer('workout3').nullable();
+    table.integer('workout4').nullable();
+    table.integer('workout5').nullable();
+    table.integer('workout6').nullable();
 
-        table.foreign('workout1').references('workout_menu.id');
-        table.foreign('workout2').references('workout_menu.id');
-        table.foreign('workout3').references('workout_menu.id');
-        table.foreign('workout4').references('workout_menu.id');
-        table.foreign('workout5').references('workout_menu.id');
-        table.foreign('workout6').references('workout_menu.id');
-      })
+    table.foreign('workout1').references('workout_menu.id');
+    table.foreign('workout2').references('workout_menu.id');
+    table.foreign('workout3').references('workout_menu.id');
+    table.foreign('workout4').references('workout_menu.id');
+    table.foreign('workout5').references('workout_menu.id');
+    table.foreign('workout6').references('workout_menu.id');
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.dropTable('workout');
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('workout');
 };
