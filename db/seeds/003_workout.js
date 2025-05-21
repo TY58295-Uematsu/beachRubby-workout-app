cd0f296a38aadb5948a15cc031062b1078fc005f
Wentz <ryo_uematsu@mail.toyota.co.jp>
2025-05-21 22:56:52 +0900

    seedファイル実行ログinsert内容ログ

--- db/seeds/003_workout.js
+++ db/seeds/003_workout.js
@@ -7,9 +7,10 @@ const initialValue = require('./../fixtures')
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
+  console.log('003seedstart');
+  console.log(initialValue);
   await knex('workout').del()
   await knex('workout').insert(initialValue);
-  console.log('003seed');
-  console.log(initialValue);
+  console.log('003seedend');
 
 };
