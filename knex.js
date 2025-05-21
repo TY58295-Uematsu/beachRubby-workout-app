d23fc314e2b10a94188809c271f0bd90f2ce2c46
Wentz <ryo_uematsu@mail.toyota.co.jp>
2025-05-21 15:59:02 +0900

    knex.js周辺修正

--- knex.js
+++ knex.js
@@ -1,5 +1,13 @@
-const knex = require('knex');
-let config = require('./knexfile')
-const db = knex(config.development);
+require('dotenv').config();
 
+const knex = require('knex');
+let config = require('./knexfile');
+
+
+if (process.env.NODE_ENV==='production') {
+    config = config.production;
+} else if (process.env.NODE_ENV==='development'){
+    config = config.development;
+}
+const db = knex(config);
 module.exports=db
